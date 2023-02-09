792f0bae49f911312c7e9c6226335d170b8dbe6e
doimiod <doim@tcd.ie>
2023-02-09 08:24:30 +0000

    basic authentication

--- react-spotify-practice/src/App.js
+++ react-spotify-practice/src/App.js
@@ -15,10 +15,14 @@ function App() {
 
     const hash = window.location.hash
     let token = window.localStorage.getItem("token")
+    setToken(token)
 
     if(!token && hash) {
 
       token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      const hash = ""
+      let token = window.localStorage.setItem("token", token)
 
       console.log(token)
     }
@@ -37,10 +41,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Spotify React</h1>
-
-
+        {!token ?
         <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-        <button onClick={logout}>Logout</button>
+        :<button onClick={logout}>Logout</button>}
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
